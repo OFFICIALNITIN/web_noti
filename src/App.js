1c38c4458f217a1ded0be0d4c995892b82f304a4
OFFICIALNITIN <official.nitin105@gmail.com>
2024-06-13 21:37:14 +0530

    added firebase notification app demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  async function requestPermission() {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        // Generate token
+        const token = await getToken(messaging, {
+          vapidKey: "",
+        });
+        console.log("FCM Token:", token);
+      } else if (permission === "denied") {
+        alert("You denied the notification permission");
+      }
+    } catch (error) {
+      console.error("Error getting permission or token:", error);
+    }
+  }
+
+  useEffect(() => {
+    async function registerServiceWorker() {
+      try {
+        const registration = await navigator.serviceWorker.register(
+          "/firebase-messaging-sw.js"
+        );
+        console.log(
+          "Service Worker registration successful with scope: ",
+          registration.scope
+        );
+
+        // Request notification permission
+        await requestPermission();
+      } catch (err) {
+        console.error("Service Worker registration failed: ", err);
+      }
+    }
+
+    registerServiceWorker();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
